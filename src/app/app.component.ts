d1bd87eb0441bf8fdced5180e012411afe8b37c2
Minh Truong <minh@truong.fi>
2022-02-12 11:45:21 +0200

    Add navigation and multipage support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,5 @@
 import { Component } from '@angular/core';
 
-
-export interface MoiMobiiliReilu5G {
-  plan: number | string;
-  dataAmount4G: number;
-  dataAmount5G: number;
-  calls: number;
-  sms: number;
-}
-
-export interface MoiMobiiliReilu5GOutput {
-  monthlyCostSummary: number;
-  monthlyCostData: number;
-  monthlyCostCallSMS: number;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -22,59 +7,5 @@ export interface MoiMobiiliReilu5GOutput {
 })
 export class AppComponent {
 
-  ngOnInit() {
-    this.MoiMobileReilu5G()
-  }
-
-
-  formMoiMobiiliReilu5G: MoiMobiiliReilu5G = {
-    plan: '400',
-    dataAmount4G: 0,
-    dataAmount5G: 0,
-    calls: 0,
-    sms: 0
-  }
-
-  formMoiMobiiliReilu5GOutput: MoiMobiiliReilu5GOutput = {
-    monthlyCostSummary: 0,
-    monthlyCostData: 0,
-    monthlyCostCallSMS: 0,
-  }
-
-
-  MoiMobileReilu5G() {
-
-    //5G data price max
-    let dataPrice5G = 8;
-
-    // 400M speed price in 4G
-    let dataprice4G = 19;
-    let dataMaxPrice5G = 27;
-    if(this.formMoiMobiiliReilu5G.plan == 1000) {
-      // 1000M speed price in 4G
-      dataprice4G = 35
-      dataMaxPrice5G = 43;
-    }
-
-    let monthlyCallSMSPrice = 0.055 * (this.formMoiMobiiliReilu5G.calls + this.formMoiMobiiliReilu5G.sms);
-
-    let monthyDataPrice = dataprice4G + (dataPrice5G * (this.formMoiMobiiliReilu5G.dataAmount5G / (this.formMoiMobiiliReilu5G.dataAmount5G + this.formMoiMobiiliReilu5G.dataAmount4G)));
-
-    if(monthyDataPrice > dataMaxPrice5G)
-      monthyDataPrice = dataMaxPrice5G;
-
-    if(this.formMoiMobiiliReilu5G.dataAmount4G == 0 && this.formMoiMobiiliReilu5G.dataAmount5G == 0)
-      monthyDataPrice = dataprice4G;
-
-    // SMS & Call price max 4€
-    if(monthlyCallSMSPrice > 4)
-      monthlyCallSMSPrice = 4;
-
-    this.formMoiMobiiliReilu5GOutput = {
-      monthlyCostSummary: (monthlyCallSMSPrice + monthyDataPrice),
-      monthlyCostData: monthyDataPrice,
-      monthlyCostCallSMS: monthlyCallSMSPrice
-    }
-  }
 
 }
